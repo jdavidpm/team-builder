aeb95e7a8e8a2fc4b247fe8c3c935e328d1f4e78
jdavidpm <97.david.pm@gmail.com>
2021-04-30 21:38:12 -0500

    Minor changes

--- teambuilder/settings.py
+++ teambuilder/settings.py
@@ -149,5 +149,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ssiet.manager@gmail.com'#config['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = 'xesgmyhierldokfn'#config['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER = ''#config['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = ''#config['EMAIL_HOST_PASSWORD']
