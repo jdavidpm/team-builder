0ccb7c33fbc53c86f7c8bf36900d12391353baf4
santi4o <yinyue.sant@gmail.com>
2021-05-14 23:55:34 -0500

    all modules in tasks working

--- projects/urls.py
+++ projects/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('projects/create', views.projects_create, name='projects-create'),
     path('projects/<int:id>/', views.project_item, name='projects-item'),
     path('projects/<int:id>/update/', views.project_update, name='projects-update'),
-    path('projects/tasks/', views.tasks, name='projects-tasks'),
-    path('projects/tasks/<str:filter_by>/<int:object_id>/', views.filtered_tasks, name='projects-filteredtasks'),
+    path('tasks/', views.tasks, name='projects-tasks'),
+    path('tasks/<str:filter_by>/<int:object_id>/', views.filtered_tasks, name='projects-filteredtasks'),
+    path('tasks/new/<int:project_id>', views.createTask, name='projects-create_task'),
+    path('tasks/<int:pk>/', views.updateTask, name='projects-update_task'),
+    path('tasks/<int:pk>/delete/', views.deleteTask, name='projects-delete_task'),
 ]
