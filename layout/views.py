bb61dfaebe393c136dd08e890622d417fe919194
jdavidpm <97.david.pm@gmail.com>
2021-05-17 20:41:53 -0500

    Personality test results added

--- layout/views.py
+++ layout/views.py
@@ -1,3 +1,4 @@
+from django.db.models import query
 from django.shortcuts import redirect, render
 from users.models import Task
 from .forms import PersonalityTestForm
@@ -26,20 +27,34 @@ def tools(request):
 
 def hexaco_test(request):
 	dict_facets = {'H':0, 'E':0, 'X':0, 'A':0, 'C':0, 'O':0}
+	list_values = [1, 2, 3, 4, 5]
 	if request.method == 'POST':
 		form = PersonalityTestForm(request.POST)
 		if form.is_valid():
 			for i in range(1, 61):
 				facet, sub_facet, is_reversed = data[i - 1]['statement_facet'].split(', ')
-				dict_facets[facet] += int(form.cleaned_data['statement_' + str(i)])
+				statement_value = int(form.cleaned_data['statement_' + str(i)])
+				dict_facets[facet] += list_values[-statement_value] if is_reversed == 'R' else statement_value
 			dict_facets = {f: dict_facets[f] / 10 for f in dict_facets}
 			current_user = request.user
 			for f in dict_facets:
 				setattr(current_user.profile, 'personality_' + f.lower(), dict_facets[f])
 			current_user.save()
-			return redirect('layout-index')
-
-	# if a GET (or any other method) we'll create a blank form
+			return redirect('layout-hexaco-results')
 	else:
 		form = PersonalityTestForm()
 	return render(request, 'layout/hexaco_test.html', {'title': 'Test de Personalidad', 'form': form})
+
+def hexaco_results(request):
+	hexaco_caps = 'hexaco'
+	chart_type = request.GET.get('type')
+	labels = ['Honestidad', 'Emoción', 'Extraversión', 'Amabilidad', 'Escrupulosidad', 'Apertura']
+	values = [getattr(request.user.profile, 'personality_' + c) for c in hexaco_caps]
+	context = {
+		'title': 'Resultados HEXACO',
+		'labels': labels,
+		'values': values,
+		'type': chart_type if chart_type else 'polarArea',
+		'bool': False 
+	}
+	return render(request, 'layout/hexaco_results.html', context)
